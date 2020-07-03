beb11400b9e0949b008ea06770055fbca4bff256
Luis Robledo <larobledog@gmail.com>
2020-07-03 14:21:29 -0500

    Elaboracion del formulario Contratistas clases y diseño

---
+++ Controllers/ContratistasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConcursosContratos.Controllers
+{
+    public class ContratistasController : Controller
+    {
+        // GET: Contratistas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ContratistaCLS.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConcursosContratos.Models
+{
+    public class ContratistaCLS
+    {
+        public string Rfc
+        {
+            get;set;
+        }
+
+        public string RazonSocial
+        {
+            get;set;
+        }
+
+        public string Telefono
+        {
+            get;set;
+        }
+
+        public string Fax
+        {
+            get;set;
+        }
+
+        public string Calle
+        {
+            get;set;
+        }
+
+        public string NoExterior
+        {
+            get;set;
+        }
+
+        public string NoInterior
+        {
+            get;set;        
+        }
+
+        public string Colonia
+        {
+            get;set;
+        }
+
+        public string Cp
+        {
+            get;set;
+        }
+
+        public string Curp
+        {
+            get;set;
+        }
+
+        public int IdMunicipio
+        {
+            get;set;
+        }
+
+        public string Correo
+        {
+            get;set;
+        }
+
+        public int Año
+        {
+            get;set;
+        }
+    }
+}
